bdb9e87a854f27765c3e015e66fe68a7b7de1270
ray.chai <ray.chai@icloud.com>
2024-06-11 22:48:15 +0800

    commit

---
+++ bluetooth/ColorGridView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorGridView.swift
+//  bluetooth
+//
+//  Created by Ray chai on 2024/6/11.
+//
+
+import SwiftUI
+
+struct ColorGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorGridView()
+}
